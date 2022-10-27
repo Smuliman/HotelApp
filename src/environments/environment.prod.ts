f8e30eb2215922514d294da349af9327f061d396
Smuliman <samuli.tyyska@live.com>
2022-10-27 20:42:52 +0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'hotelproject-721c0',
+    appId: '1:990148863157:web:3586e734dedd632adacafc',
+    storageBucket: 'hotelproject-721c0.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCyr59KeUo4OgkJcUlvxHOHnrkMIEUF0XQ',
+    authDomain: 'hotelproject-721c0.firebaseapp.com',
+    messagingSenderId: '990148863157',
+  },
   production: true
 };
